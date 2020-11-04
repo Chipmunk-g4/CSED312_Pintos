3ea5330b83adbdcca380736ae745b3f03dbf0e0a
jundol990510 <jundol990510@postech.ac.kr>
2020-11-04 11:19:27 +0900

    clean code

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -10,7 +10,11 @@ struct semaphore
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
   };
-
+struct semaphore_elem 
+  {
+    struct list_elem elem;              /* List element. */
+    struct semaphore semaphore;         /* This semaphore. */
+  };
 void sema_init (struct semaphore *, unsigned value);
 void sema_down (struct semaphore *);
 bool sema_try_down (struct semaphore *);
@@ -22,6 +26,7 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem elem;
   };
 
 void lock_init (struct lock *);
@@ -40,10 +45,6 @@ void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
-
-bool cmp_sem_front_priority(struct list_elem *a, struct list_elem *b, void *aux);
-
-void donate_priority_with_nested(struct lock *l);
 
 /* Optimization barrier.
 
