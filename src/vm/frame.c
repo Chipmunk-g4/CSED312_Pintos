04ce9be08a13bb261a20996d052199dfabb00017
Eunchan Jo <eunchan9029@postech.ac.kr>
2020-12-10 13:47:13 +0900

    get_next_lru_clock 함수 추가

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -5,6 +5,9 @@
 #include "threads/interrupt.h"
 #include "threads/vaddr.h"
 
+// Clock 알고리즘을 수행할 때 lru_list의 다음 원소로 이동하는 역할을 하는 함수이다.
+static struct list_elem *get_next_lru_clock ();
+
 // lru_list를 초기화하는 함수
 void lru_list_init (void){
     list_init (&lru_list);
@@ -24,4 +27,38 @@ void delete_page_lru (struct page* page){
     // 만약 page가 lru_clock으로 되어있는 경우, lru_clock을 변경한다.
     if(lru_clock == &page->lru_elem)    lru_clock = list_remove (lru_clock);
 	else                                list_remove(&page->lru_elem);
+}
+
+// Clock 알고리즘을 수행할 때 lru_list의 다음 원소로 이동하는 역할을 하는 함수이다.
+// 호출시 lru_list에서 다음 위치를 반환한다.
+static struct list_elem *get_next_lru_clock (){
+
+    // 만약 현재 lru_clock이 비어있는 경우
+    if(lru_clock == NULL){
+        // 만약 lru_list가 비어있는 경우 NULL반환
+        if(list_empty (&lru_list)){
+            return NULL;
+        }
+        // 그렇지 않은 경우 lru_list의 첫번째 원소를 반환
+        else{
+            return (lru_clock = list_begin(&lru_list));
+        }
+    }
+
+    // 다음 원소로 이동
+    struct list_elem *e = list_next(lru_clock);
+
+    // 만약 lru_clock이 lru_list의 마지막에 도착한 경우
+    if(e == list_end(&lru_list)){
+        // 만약 lru_list에 페이지가 한개만 있는 경우 NULL반환
+		if(lru_clock == list_begin(&lru_list)){
+			return NULL;
+		}
+        // 그렇지 않은 경우 lru_list의 시작지점 설정
+		else{
+			e = list_begin(&lru_list);
+		}
+    }
+    lru_clock = e;
+    return e;
 }
